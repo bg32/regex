2bf83325447c12f74a502a8c6fca02fbcad6f237
Bethany Gillam <bethany@wearesavvy.com>
2019-09-25 23:40:36 +0100

    commit all

---
+++ src/reg/tes.java
@@ -0,0 +1,12 @@
+package reg;
+
+public class tes {
+	public static void main(String[] args) {
+		otherTest ot = new otherTest ();
+		ot.getRegEx("The brown COM0999");
+		ot.getRegEx("FOO BAR W9999");
+		ot.getRegEx("THE FOO BAR J9839");
+		ot.getRegEx("The BAR FOO HB8949");
+	}
+
+}

4597d6229cd6fafa23d735288d905eae9e5baa05
Bethany Gillam <bethany@wearesavvy.com>
2019-09-27 21:56:29 +0100

    fix: stop false strings getting through

--- src/reg/otherTest.java
+++ src/reg/otherTest.java
@@ -3,21 +3,17 @@ package reg;
 
 public class otherTest {
 
-
 public String getRegEx(String arg) {
 	String [] strArray = arg.split(" ");
 	for (String s: strArray) {
-		if (s.matches("[J, W, COM, HB]+[0-9]{4}")){
+		if (s.matches("(J|W|COM|HB)[0-9]{4}")){
 			System.out.println(s);
 			return s;
+
 		}
 	}
 	return null;
-		
-	
-			
+
 }
 
-
-	
 }
